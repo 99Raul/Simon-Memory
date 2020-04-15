4b5939fac7b08708d9c2032b741bd01c5d83c48c
99Raul <rauljr96@gmail.com>
2020-04-14 17:28:19 -0700

    added a how to play button, working on bonus

--- script.js
+++ script.js
@@ -91,9 +91,41 @@ const startFlashing = async () => {
 // prevents you from clicking until flash sequence ends
 startFlashing();
 
-// reset the game board
+// reset the game
 const reset = document.querySelector('.reset');
 reset.addEventListener('click', onReset);
 function onReset() {
 	location.reload();
 }
+
+//------------------------------------------------------------------------------------
+// How To play button
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+	modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
+};
+
+//-----------------------------------------------------------------------------------
+//Bonus - Tracking points/score
+const $level_ = document.querySelector('#level>span');
